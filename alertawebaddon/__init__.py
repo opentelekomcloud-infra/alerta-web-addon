46ca265b81d4276545efe0279211084baba6a4cb
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-07-02 17:28:56 +0300

    move config variables to env vars

--- alertawebaddon/__init__.py
+++ alertawebaddon/__init__.py
@@ -6,17 +6,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 AGP = ArgumentParser(prog='Web server for alerta', description='')
 AGP.add_argument('--port', help='port to be listened', default=23456, type=int)
-AGP.add_argument('--dbstring', help='Database connection string', type=str)
 AGP.add_argument('--debug', help='debug mode', default=False, type=bool)
-AGP.add_argument('--ldap_domain', help='debug mode', default=False, type=str)
 args, _ = AGP.parse_known_args()
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = args.dbstring
-app.config['WTF_CSRF_SECRET_KEY'] = 'csrf'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['GITHUB_OAUTH_CLIENT_ID'] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
 app.config['GITHUB_OAUTH_CLIENT_SECRET'] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
 app.secret_key = os.environ.get("APP_SECRET_KEY")
+app.config['WTF_CSRF_SECRET_KEY'] = 'csrf'
 
 db = SQLAlchemy(app)
