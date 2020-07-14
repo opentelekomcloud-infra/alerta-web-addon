c7fff10de1a5e9aee01ac96d15ba76d9adbb1d99
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-07-14 15:57:36 +0300

    app.register_blueprint(github_bp, url_prefix="/login")

--- alertawebaddon/__main__.py
+++ alertawebaddon/__main__.py
@@ -6,7 +6,7 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from alertawebaddon import app, args
 from alertawebaddon.views import github_bp
 
-app.register_blueprint(github_bp, url_prefix="/webaddon")
+app.register_blueprint(github_bp, url_prefix="/login")
 from flask_bootstrap import Bootstrap
 
 Bootstrap(app)
