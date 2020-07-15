aecacf4554666797427bf7fafbd12e55cb92b1bc
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-07-15 12:55:16 +0300

    reverse proxify

--- alertawebaddon/__main__.py
+++ alertawebaddon/__main__.py
@@ -3,6 +3,18 @@ from threading import Thread
 from alertawebaddon import app, args
 from alertawebaddon.views import github_bp
 
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        scheme = environ.get('HTTP_X_FORWARDED_PROTO')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+app.wsgi_app = ReverseProxied(app.wsgi_app)
+
 app.register_blueprint(github_bp, url_prefix="/webaddon/login")
 from flask_bootstrap import Bootstrap
 
