7becebfecc545ce4a3e0e8fc40380e30de009576
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-07-15 16:03:04 +0300

    trying to use ProxyFix

--- alertawebaddon/__init__.py
+++ alertawebaddon/__init__.py
@@ -3,31 +3,31 @@ from argparse import ArgumentParser
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.contrib.fixers import ProxyFix
 
-
-class ReverseProxied(object):
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        environ['wsgi.url_scheme'] = 'https'
-        return self.app(environ, start_response)
-
-class PrefixMiddleware(object):
-
-    def __init__(self, app, prefix=''):
-        self.app = app
-        self.prefix = prefix
-
-    def __call__(self, environ, start_response):
-
-        if environ['PATH_INFO'].startswith(self.prefix):
-            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
-            environ['SCRIPT_NAME'] = self.prefix
-            return self.app(environ, start_response)
-        else:
-            start_response('404', [('Content-Type', 'text/plain')])
-            return ["This url does not belong to the app.".encode()]
+# class ReverseProxied(object):
+#     def __init__(self, app):
+#         self.app = app
+#
+#     def __call__(self, environ, start_response):
+#         environ['wsgi.url_scheme'] = 'https'
+#         return self.app(environ, start_response)
+#
+# class PrefixMiddleware(object):
+#
+#     def __init__(self, app, prefix=''):
+#         self.app = app
+#         self.prefix = prefix
+#
+#     def __call__(self, environ, start_response):
+#
+#         if environ['PATH_INFO'].startswith(self.prefix):
+#             environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+#             environ['SCRIPT_NAME'] = self.prefix
+#             return self.app(environ, start_response)
+#         else:
+#             start_response('404', [('Content-Type', 'text/plain')])
+#             return ["This url does not belong to the app.".encode()]
 
 AGP = ArgumentParser(prog='Web server for alerta', description='')
 AGP.add_argument('--port', help='port to be listened', default=23456, type=int)
@@ -53,6 +53,8 @@ app.config['GITHUB_OAUTH_ALLOWED_ORGANIZATIONS'] = \
 app.secret_key = os.environ.get("APP_SECRET_KEY")
 app.config['WTF_CSRF_SECRET_KEY'] = 'csrf'
 
-app.wsgi_app = ReverseProxied(app.wsgi_app)
-app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/webaddon')
+app.wsgi_app = ProxyFix(app.wsgi_app)
+# app.wsgi_app = ReverseProxied(app.wsgi_app)
+# app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/webaddon')
+
 db = SQLAlchemy(app)
