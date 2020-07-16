15d9f21e43687580a9209935cd8128eaadb2348a
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-07-16 17:02:42 +0300

    github bp redirect

--- alertawebaddon/views.py
+++ alertawebaddon/views.py
@@ -9,7 +9,7 @@ from alertawebaddon.forms import EnvUpdateForm, TopicUpdateForm, TemplateUpdateF
 from alertawebaddon.model import Environments, Topics, Templates, TopicsToSkip, get_last_id
 
 db.create_all()
-github_bp = make_github_blueprint(redirect_to=app.config['PROXY_PREFIX_PATH'])
+github_bp = make_github_blueprint()
 
 
 @app.route('/', methods=['GET'])
